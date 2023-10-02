06101c40b59f92e2c7d3be0008318f169f7760e8
Ivana Arbutina <ivana.arbutina2@gmail.com>
2023-10-03 01:11:50 +0200

    add footer-info

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,18 +1,31 @@
 import Contact from "./contact";
 import { useTranslation } from "react-i18next";
+import heart from "./../../assets/icons/icons8-heart-94.png";
 
 const Footer = () => {
   const { t } = useTranslation();
   return (
     <div className="footer">
       <Contact />
+      <p className="footer__info">
+        Made with
+        <img src={heart} />
+        by Ivana Arbutina
+      </p>
       <div className="footer__link">
         <span>{t("freepik")}</span>
         <a
-          href="https://www.freepik.com/free-vector/gradient-black-backgrounds-with-golden-frames_19852263.htm#from_view=detail_serie"
+          href="https://www.freepik.com/free-ai-image/bottle-perfume-with-red-substance-it_41203007.htm#query=parfume&position=1&from_view=search&track=sph"
           className="footer__link__item"
         >
-          Freepik
+          Sketchepedia
+        </a>
+        <span> & </span>
+        <a
+          href="https://www.freepik.com/free-ai-image/charming-pampered-pets-play-side-by-side-looking-alert-generated-by-ai_41151542.htm#&position=35&from_view=author"
+          className="footer__link__item"
+        >
+          Vecstock
         </a>
       </div>
     </div>
